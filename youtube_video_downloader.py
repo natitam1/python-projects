67beaa2d865e187b9c40ee5f42fc56fe8d08be79
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-10-03 22:17:44 +0300

    feat(downloader): add youtube video downloader

---
+++ youtube_video_downloader.py
@@ -0,0 +1,20 @@
+from pytube import YouTube
+import tkinter as tk
+from tkinter import filedialog
+
+def download_video(url, save_path):
+    try:
+        yt = YouTube(url)
+        highest_res_stream = yt.streams.filter(progressive=True, file_extension="mp4").get_highest_resolution()
+
+        highest_res_stream.download(output_path=save_path)
+       
+        print("Video downloaded successfully!")
+
+    except Exception as e:
+        print(e)
+
+url="https://www.youtube.com/watch?v=CJkDKIsvROM&pp=0gcJCfsJAYcqIYzv"
+save_path = "C:/Users/lead/Desktop/python-projects"
+
+download_video(url, save_path)
