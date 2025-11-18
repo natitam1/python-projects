0d3b1810cac52b9c985f5cd8b24d8fa6a08d8e1e
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-11-18 10:52:42 +0300

    chore: add a qr code generator

--- env/test.py
+++ env/test.py
@@ -1,3 +1,13 @@
-import cowsay
+my_arr =[14, 23, 51, 15, 155]
+n = len(my_arr)
+for i in range(n - 1):
+    swapped = False
+    for j in range(n - i - 1):
+        if my_arr[j] > my_arr[j+1]:
+            my_arr[j], my_arr[j+1] = my_arr[j+1], my_arr[j]
+            
+            swapped = True
+        if not swapped:
+            break
 
-print(cowsay.cow("Hi this is Natnael"))
+print(my_arr)
